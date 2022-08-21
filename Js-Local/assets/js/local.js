bea25abc3492dc309eefe20f5d82756b78f1e7eb
RamilBinnatov <ramilrb@code.edu.az>
2022-08-22 00:27:32 +0400

    Js-function

---
+++ Js-Local/assets/js/local.js
@@ -0,0 +1,101 @@
+//1. Division
+
+// CheckNum(25)
+
+// function CheckNum(num) {
+//     if (num % 21 == 0) {
+//         console.log("Is dividable");
+//     }
+//     else {
+//         console.log("Undividable");
+//     }
+// }
+// CheckNum(25)
+
+
+//2. Factorial
+
+// let result = 1;
+
+// function Factorial(num) {
+//     if (num < 0) {
+//         console.log("Please enter positive number");
+//     }
+//     else {
+//         for (let i = 1; i <= num; i++) {
+//             if (num > 0) {
+//                 result *= i;
+//             }
+//         }
+//         console.log(result);
+//     }
+// }
+// Factorial(3)
+
+
+// 3. even num square
+
+// let nums = [1, 2, 3, 4, 5, 6];
+// let sum = 0;
+
+// function Sum(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if (arr[i] % 2 == 0) {
+//             sum += Math.pow(arr[i], 2);
+//         }
+//     }
+//     console.log(sum);
+// }
+
+// Sum(nums)
+
+
+// 4. authentication
+
+// let mail = "ramilrb@code.edu.az";
+// let password = "12345";
+
+// function CheckMail(email, pass) {
+//     if (email == mail && pass == password) {
+//         console.log("Giris olundu");
+//     }
+//     else {
+//         console.log("Mail ve Passowrd sehvdir")
+//     }
+// }
+// CheckMail("ramilrb@code.edu.az", "12345")
+
+//5. odd nums sum
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let sum = 0;
+
+// function SumOfodds(nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] % 2 != 0) {
+//             sum += nums[i]
+//         }
+//     }
+//     console.log(sum);
+// }
+
+// SumOfOdds(arr);
+
+//6. even nums sum
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let count = 0;
+// function CountOfevens(nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] % 2 == 0) {
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+// CountOfevens(arr);
+
+
+
